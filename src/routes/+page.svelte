c21fa14df677937c057b2a94519a43b4d01631a4
Jason Sturges <jason@jasonsturges.com>
2022-09-02 22:56:56 -0500

    SvelteKit implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import { createScene } from '../lib/scene.js';
+
+	let el;
+
+	onMount(() => {
+		createScene(el);
+	});
+</script>
+
+<svelte:head>
+	<title>Three.js Svelte Boilerplate</title>
+	<meta name="description" content="Three.js example app built with Svelte" />
+</svelte:head>
+
+<canvas bind:this={el} />
