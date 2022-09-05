948ea16bfc27293a6340c70b4435ba2f7a2eaa3f
Jason Sturges <jason@jasonsturges.com>
2022-09-04 20:52:20 -0500

    Refactoring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <svelte:head>
-	<title>Three.js Svelte Boilerplate</title>
+	<title>Three.js Sveltekit</title>
 	<meta name="description" content="Three.js example app built with Svelte" />
 </svelte:head>
 
