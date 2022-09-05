1024347d0604aec533edd93a1ebc43eced59986f
Jason Sturges <jason@jasonsturges.com>
2022-09-04 20:55:14 -0500

    Static site configuration for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,29 @@
-import adapter from '@sveltejs/adapter-auto';
+/** @type {import('@sveltejs/kit').Config} */
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: '/threejs-sveltekit'
+    },
+    prerender: {
+      // This can be false if you're using a fallback (i.e. SPA mode)
+    }
+  },
 
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: [
+    preprocess({
+      postcss: true
+    })
+  ]
 };
 
 export default config;
