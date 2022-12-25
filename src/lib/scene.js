c258db50c89de5700804aede0336396e4ba1ea33
bforbiggy <AndyHeCS@protonmail.com>
2022-12-25 18:38:35 -0500

    Include Cube Shadows

--- src/lib/scene.js
+++ src/lib/scene.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 let renderer;
 scene.add(cube);
