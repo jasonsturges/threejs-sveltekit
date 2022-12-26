160dea6a75cff697b6dcf5a6987af3e74fdcd22a
Jason Sturges <jason@jasonsturges.com>
2022-12-26 14:27:53 -0600

    Scene lighting and material change

--- src/lib/scene.js
+++ src/lib/scene.js
@@ -1,18 +1,29 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, metalness: 0.1 });
-const cube = new THREE.Mesh(geometry, material);
-let renderer;
-scene.add(cube);
 camera.position.z = 5;
 
-// Add lighting
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, 0, 50);
-scene.add(light);
+const geometry = new THREE.BoxGeometry();
+
+const material = new THREE.MeshStandardMaterial({
+	color: 0x00ff00,
+	metalness: 0.13
+});
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const directionalLight = new THREE.DirectionalLight(0x9090aa);
+directionalLight.position.set(-10, 10, -10).normalize();
+scene.add(directionalLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+hemisphereLight.position.set(1, 1, 1);
+scene.add(hemisphereLight);
+
+let renderer;
 
 const animate = () => {
 	requestAnimationFrame(animate);
